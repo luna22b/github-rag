de55820a4ba775725d238dc437c653ead9a80638
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-20 14:39:18 -0400

    Backend app folder - Added SQLAlchemy code to connect to postgres

---
+++ backend/app/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.database.database import engine, Base
+from app.routes import users
+
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(
+    users.router,
+    prefix="/users",
+    tags=["Users"]
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "GitHub RAG API is running"
+    }
+
