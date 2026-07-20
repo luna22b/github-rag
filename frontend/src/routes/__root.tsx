092e031d90f2bc8433c7e1888bbdd535aaaa6c43
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-20 16:05:29 -0400

    Added a navbar on basically every page by placing the component in the root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,28 +1,30 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import Navbar from "#/components/Navbar";
 
-import '../styles.css'
+import "../styles.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
+      <Navbar />
       <Outlet />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'TanStack Router',
+            name: "TanStack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
-  )
+  );
 }
