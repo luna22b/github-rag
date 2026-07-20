8f426940439a7f6bbe0b26dd05a1489c519fe965
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-20 16:06:39 -0400

    Homepage - Added components

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Header from "#/components/Header";
+import ButtonsHeader from "#/components/ButtonsHeader";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <ButtonsHeader />
+    </div>
+  );
 }
