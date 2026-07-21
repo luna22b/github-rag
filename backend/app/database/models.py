55317496db45cdb7c0e5510a52196e98dc25db30
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-21 02:49:21 -0400

    Modified the model by making the username unique and changing the password to fix code

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -9,21 +9,23 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(
         primary_key=True,
-        index=True
+        index=True,
     )
 
     username: Mapped[str] = mapped_column(
         String(50),
-        nullable=False
+        unique=True,
+        nullable=False,
     )
 
     email: Mapped[str] = mapped_column(
         String(100),
         unique=True,
-        nullable=False
+        nullable=False,
+        index=True,
     )
 
-    password_hash: Mapped[str] = mapped_column(
+    hashed_password: Mapped[str] = mapped_column(
         String(255),
-        nullable=False
+        nullable=False,
     )
