7590378689f8c2afbca9280db5c7c8fb9e1b12d9
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-21 02:59:24 -0400

    Modified the navbar so that it uses queries and will display logout if the user is logged in

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,28 +1,82 @@
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "@tanstack/react-router";
+import { useQueryClient } from "@tanstack/react-query";
+import { logoutUser } from "../api/auth";
+
+import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { data: user, isLoading } = useCurrentUser();
+
+  async function handleLogout() {
+    const success = await logoutUser();
+
+    if (!success) {
+      return;
+    }
+
+    await queryClient.invalidateQueries({
+      queryKey: ["currentUser"],
+    });
+
+    navigate({
+      to: "/",
+    });
+  }
+
+  function handleLogoClick() {
+    if (user) {
+      navigate({
+        to: "/dashboard",
+      });
+    } else {
+      navigate({
+        to: "/",
+      });
+    }
+  }
 
   return (
     <nav>
       <div className="flex justify-between max-w-7xl mx-auto px-6 h-14 items-center border-b border-b-[oklch(0.27_0.005_285)]">
-        <p className="text-white font-semibold text-md">MD</p>
-        <div className="flex gap-4 text-xs items-center">
-          <button
-            className="text-white cursor-pointer"
-            onClick={() => navigate({ to: "/login" })}
-          >
-            Sign in
-          </button>
+        <p
+          className="text-white font-semibold text-md cursor-pointer"
+          onClick={handleLogoClick}
+        >
+          MD
+        </p>
 
-          <button
-            className="text-black rounded-lg px-3 py-1.5 flex justify-center items-center gap-1 bg-[oklch(0.965_0.002_285)] cursor-pointer"
-            onClick={() => navigate({ to: "/signup" })}
-          >
-            Get Started
-            <ArrowRight size={12} />
-          </button>
+        <div className="flex gap-4 text-xs items-center">
+          {!isLoading && user ? (
+            <button
+              className="text-black rounded-lg px-3 py-1.5 bg-[oklch(0.965_0.002_285)] cursor-pointer"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          ) : (
+            !isLoading && (
+              <>
+                <button
+                  className="text-white cursor-pointer"
+                  onClick={() => navigate({ to: "/login" })}
+                >
+                  Sign in
+                </button>
+
+                <button
+                  className="text-black rounded-lg px-3 py-1.5 flex justify-center items-center gap-1 bg-[oklch(0.965_0.002_285)] cursor-pointer"
+                  onClick={() => navigate({ to: "/signup" })}
+                >
+                  Get Started
+                  <ArrowRight size={12} />
+                </button>
+              </>
+            )
+          )}
         </div>
       </div>
     </nav>
