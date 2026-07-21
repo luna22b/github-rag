8aa8f2747d4c9a9a4b3e7616164153aebe8ad3d1
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-21 02:50:11 -0400

    New api folder for logout and middleware to check if user is authenticated

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export async function getCurrentUser() {
+  try {
+    const response = await axios.get("http://localhost:8000/api/me", {
+      withCredentials: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function logoutUser() {
+  try {
+    await axios.post(
+      "http://localhost:8000/api/logout",
+      {},
+      {
+        withCredentials: true,
+      },
+    );
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
