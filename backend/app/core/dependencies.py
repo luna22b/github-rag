91c7dc07e2599019d6a258f8877a80801637535e
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-21 21:00:30 -0400

    Moved some items out of the dependencies folder for better file layout

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,34 +1,29 @@
-from fastapi import Depends, HTTPException, status
-from fastapi import Request
+from fastapi import Depends, HTTPException, Request
 from jose import jwt, JWTError
-
-from app.database.database import get_db
-from app.database.models import User
 from sqlalchemy.orm import Session
 
-from app.core.security import (
-    SECRET_KEY,
-    ALGORITHM,
-)
+from app.core.config import settings
+from app.database.database import get_db
+from app.database.models import User
 
 
 def authenticate_user(
     request: Request,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ):
     token = request.cookies.get("access_token")
 
     if not token:
         raise HTTPException(
             status_code=401,
-            detail="Not authenticated"
+            detail="Not authenticated",
         )
 
     try:
         payload = jwt.decode(
             token,
-            SECRET_KEY,
-            algorithms=[ALGORITHM]
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
         )
 
         user_id = payload.get("sub")
@@ -36,13 +31,13 @@ def authenticate_user(
         if user_id is None:
             raise HTTPException(
                 status_code=401,
-                detail="Invalid token"
+                detail="Invalid token",
             )
 
     except JWTError:
         raise HTTPException(
             status_code=401,
-            detail="Invalid token"
+            detail="Invalid token",
         )
 
     user = (
@@ -51,10 +46,10 @@ def authenticate_user(
         .first()
     )
 
-    if not user:
+    if user is None:
         raise HTTPException(
             status_code=404,
-            detail="User not found"
+            detail="User not found",
         )
 
     return user
