15dfa42103eb0e564dd16219f85d536f66307dff
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-23 20:06:43 -0400

    nothing changed

--- frontend/src/routes/_protected/dashboard.tsx
+++ frontend/src/routes/_protected/dashboard.tsx
@@ -1,12 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Sidebar from "#/components/Sidebar";
 
 export const Route = createFileRoute("/_protected/dashboard")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { user } = Route.useRouteContext() as { user: { username: string } };
-
   return <div className="flex bg-[#09090B]"></div>;
 }
