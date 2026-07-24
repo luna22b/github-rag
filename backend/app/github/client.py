6be9e6fc88e4529a68512e2d67fcc16e1f5478d6
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-23 20:02:53 -0400

    Handles github requests using their api

--- backend/app/github/client.py
+++ backend/app/github/client.py
@@ -1,14 +1,66 @@
+import base64
 import httpx
+
+
+GITHUB_API = "https://api.github.com"
+
 
 async def get_repositories(access_token: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            "https://api.github.com/user/repos",
+            f"{GITHUB_API}/user/repos",
             headers={
                 "Authorization": f"Bearer {access_token}",
                 "Accept": "application/vnd.github+json",
             },
         )
 
-        response.raise_for_status()
-        return response.json()
+    response.raise_for_status()
+
+    return response.json()
+
+
+async def get_tree(
+    access_token: str,
+    full_name: str,
+    branch: str,
+):
+    url = f"{GITHUB_API}/repos/{full_name}/git/trees/{branch}?recursive=1"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url,
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/vnd.github+json",
+            },
+        )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+async def get_blob(
+    access_token: str,
+    full_name: str,
+    sha: str,
+):
+    url = f"{GITHUB_API}/repos/{full_name}/git/blobs/{sha}"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url,
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/vnd.github+json",
+            },
+        )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    return base64.b64decode(
+        data["content"]
+    ).decode("utf-8")
