56e4e8e9fbd859329654d64c7162f2a35309f4d5
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-26 13:06:10 -0400

    allows decryption and encryption github access tokens stores in the database

--- backend/app/core/encryption.py
+++ backend/app/core/encryption.py
@@ -2,19 +2,12 @@ from cryptography.fernet import Fernet
 
 from app.core.config import settings
 
-
-cipher = Fernet(
-    settings.ENCRYPTION_KEY.encode()
-)
+cipher = Fernet(settings.ENCRYPTION_KEY.encode())
 
 
 def encrypt_token(token: str) -> str:
-    return cipher.encrypt(
-        token.encode()
-    ).decode()
+    return cipher.encrypt(token.encode()).decode()
 
 
 def decrypt_token(token: str) -> str:
-    return cipher.decrypt(
-        token.encode()
-    ).decode()
+    return cipher.decrypt(token.encode()).decode()
