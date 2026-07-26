9c1cb5a0f5501b89a643f65169a21ee04c794910
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-26 12:54:57 -0400

    Changed so that if the user tries to navigate to the homepage (the page you get when not logged in), it navigates you back to the repository page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,27 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import Header from "#/components/Header";
 import ButtonsHeader from "#/components/ButtonsHeader";
+import axios from "axios";
 
-export const Route = createFileRoute("/")({ component: Home });
+export const Route = createFileRoute("/")({
+  beforeLoad: async () => {
+    try {
+      await axios.get("http://localhost:8000/api/auth/me", {
+        withCredentials: true,
+      });
+
+      throw redirect({
+        to: "/repositories",
+      });
+    } catch (error) {
+      if (error instanceof Response) {
+        throw error;
+      }
+    }
+  },
+
+  component: Home,
+});
 
 function Home() {
   return (
