c3a9af9d25cbcccd1a286251b4e6844d2fdec47f
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-26 13:05:25 -0400

    Initializes a database and creates an engine

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -11,8 +11,10 @@ engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = SessionLocal()
