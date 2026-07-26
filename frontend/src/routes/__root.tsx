9f838c9a6707da24218c9ca411c804d4b9bb530f
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-26 12:56:56 -0400

    added a notfound page to the root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,11 +2,13 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import Navbar from "#/components/Navbar";
+import NotFound from "#/components/NotFound";
 
 import "../styles.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFound,
 });
 
 function RootComponent() {
@@ -14,6 +16,7 @@ function RootComponent() {
     <>
       <Navbar />
       <Outlet />
+
       <TanStackDevtools
         config={{
           position: "bottom-right",
