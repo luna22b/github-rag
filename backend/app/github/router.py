34928e7af6f3842b8ce0bf74603c29621a90a6e4
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-26 13:02:18 -0400

    router for github api operations

--- backend/app/github/router.py
+++ backend/app/github/router.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import Session
 from app.core.dependencies import authenticate_user
 from app.database.database import get_db
 from app.database.models import User
-from app.github.schemas import RepositoryResponse
+from app.github.schemas import ImportRepositoryResponse, RepositoryResponse
 from app.github import services
 
 router = APIRouter()
+
 
 @router.get("/repos", response_model=list[RepositoryResponse])
 async def get_repositories(
@@ -25,12 +26,13 @@ async def sync_repositories(
     user: User = Depends(authenticate_user),
     db: Session = Depends(get_db),
 ):
-    return await services.sync_repositories(
+    return await services.sync_repository(
         db=db,
         user=user,
     )
 
-@router.post("/repositories/repos/{repository_id}/import")
+
+@router.post("/repos/{repository_id}/import", response_model=ImportRepositoryResponse)
 async def import_repository(
     repository_id: int,
     user: User = Depends(authenticate_user),
